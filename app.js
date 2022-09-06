f00b6e22dccff9e9f9dd00ae2c2a2c0ab80029ed
Nitin011001 <thenitinchauhan01@gmail.com>
2022-09-06 15:07:01 -0400

    MVC architecture done

--- app.js
+++ app.js
@@ -1,100 +1,26 @@
-const fs = require('fs');
 
 const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan');
+//Routers
+const tourRouter=require('./routes/tourRoutes');
+const userRouter=require('./routes/userRoutes');
+
+//middlewares
+app.use(morgan('dev'));
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server', app: 'NodeTours' });
-// });
-
-// app.post('/',(req,res)=>{
-//     res.send('You can post to this endpoint...');
-// })
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+app.use((req, res, next) => {
+  console.log('Hello from the middle ware');
+  next();
 });
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-
-  const id = Number(req.params.id);
-  const tour = tours.find((el) => el.id === id);
-
-  // if(id>tours.length)
-  if (!tour) res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
-  res.status(200).json({
-    status: 'success',
-    // results:tours.length,
-    data: {
-      tour,
-    },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign(
-    {
-      id: newId,
-    },
-    req.body
-  );
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'update tour here..',
-    },
-  });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`App ruuning on port ${port}`);
-});
+//server
+module.exports=app;
